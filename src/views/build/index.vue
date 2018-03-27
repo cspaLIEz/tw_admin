e4bc7817ddeef05373dfdd3b5fb718b33623a09e
ht1131589588 <1131589588@qq.com>
2018-03-27 23:54:58 +0800

    节目制作、简单布局

--- src/views/build/index.vue
+++ src/views/build/index.vue
@@ -5,12 +5,36 @@
 
 <template>
     <div class="build-container">
-        节目制作
+        <div class="build-header">
+            <div class="build-header-logo"> 
+                <img src="../../images/build/program_build.png" alt="">
+            </div>
+            <div class="build-header-center">
+                <p><span>节目名称：</span>普通节目-1</p>
+                <p><span>节目时长：</span>01: 32: 52</p>
+                <p><span>节目分辨率：</span>1920*1080</p>
+            </div>
+            <div class="build-header-right">
+                <a href="javascript:;">保存</a>
+                <a href="javascript:;">浏览</a>
+                <a href="javascript:;">取消</a>
+            </div>
+        </div>
+        <div class="build-main">
+            <div class="build-main-left">
+
+            </div>
+            <div class="build-main-center">
+
+            </div>
+            <div class="build-main-right">
+
+            </div>
+        </div>
     </div>
 </template>
 <script>
-export default {
-    
-}
+import index from './index';
+export default index;
 </script>
 
