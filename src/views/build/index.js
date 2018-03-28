b9a4a3c50a9e38cc8f27168c104cea5109ada4b0
ht1131589588 <1131589588@qq.com>
2018-03-29 00:08:26 +0800

    节目制作布局

--- src/views/build/index.js
+++ src/views/build/index.js
@@ -1,3 +1,40 @@
 export default {
-    
+    data(){
+        return {
+            leftCollapse:[1,2,3],
+            componentCollapse:[1],
+            programTreeData:[
+                {
+                    title: '节目名',
+                    expand: true,
+                    children: [
+                        {
+                            title: '页面1',
+                            expand: true,
+                            children: [
+                                {
+                                    title: 'text1'
+                                },
+                                {
+                                    title: 'text2'
+                                }
+                            ]
+                        },
+                        {
+                            title: '页面2',
+                            expand: true,
+                            children: [
+                                {
+                                    title: 'text1'
+                                },
+                                {
+                                    title: 'text2'
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+    }
 }
--- src/views/build/index.vue
+++ src/views/build/index.vue
@@ -22,13 +22,114 @@
         </div>
         <div class="build-main">
             <div class="build-main-left">
+                <div class="bml-header">
+                    <p></p>
+                    <img src="../../images/build/close_left.png" alt="">
+                </div>
+                <Collapse v-model="leftCollapse">
+                    <Panel name="1" class="program-tree bml-group">
+                        <div class="group-header">
+                            页面
+                            <span>
+                                <Icon type="plus-round"></Icon>
+                            </span>
+                        </div>
+                        <div slot="content">
+                            <Tree :data="programTreeData"></Tree>
+                        </div>
+                    </Panel>
+                    <Panel name="2" class="components-group bml-group">
+                        <div class="group-header">
+                            组件区
+                            <span>
+                                <Icon type="plus-round"></Icon>
+                            </span>
+                        </div>
+                        <Collapse slot="content" v-model="componentCollapse" accordion class="component-collapse">
+                            <Panel name="1">
+                                <div class="component-group-header">
+                                    基础组件
+                                    <span>
+                                        <Icon type="arrow-down-b"></Icon>
+                                    </span>
+                                </div>
+                                <div slot="content">
 
+                                </div>
+                            </Panel>
+                            <Panel name="2">
+                                <div class="component-group-header">
+                                    动态组件
+                                    <span>
+                                        <Icon type="arrow-down-b"></Icon>
+                                    </span>
+                                </div>
+                                <div slot="content">
+                                    
+                                </div>
+                            </Panel>
+                            <Panel name="3">
+                                <div class="component-group-header">
+                                    可视化组件
+                                    <span>
+                                        <Icon type="arrow-down-b"></Icon>
+                                    </span>
+                                </div>
+                                <div slot="content">
+                                    
+                                </div>
+                            </Panel>
+                        </Collapse>
+                    </Panel>
+                    <Panel name="3" class="template-group bml-group">
+                        <div class="group-header">
+                            模板区
+                            <span>
+                                <Icon type="plus-round"></Icon>
+                            </span>
+                        </div>
+                        <div slot="content" class="template-group-box">
+                            <Row>
+                                <Col span="8" class="template-group-item">
+                                    <img src="../../images/build/template.png" alt="">
+                                    <p>模板1</p>
+                                </Col>
+                                <Col span="8" class="template-group-item">
+                                    <img src="../../images/build/template.png" alt="">
+                                    <p>模板1</p>
+                                </Col>
+                                <Col span="8" class="template-group-item">
+                                    <img src="../../images/build/template.png" alt="">
+                                    <p>模板1</p>
+                                </Col>
+                                <Col span="8" class="template-group-item">
+                                    <img src="../../images/build/template.png" alt="">
+                                    <p>模板1</p>
+                                </Col>
+                                <Col span="8" class="template-group-item">
+                                    <img src="../../images/build/template.png" alt="">
+                                    <p>模板1</p>
+                                </Col>
+                                <Col span="8" class="template-group-item">
+                                    <img src="../../images/build/template.png" alt="">
+                                    <p>模板1</p>
+                                </Col>
+                            </Row>
+                        </div>
+                    </Panel>
+                </Collapse>
             </div>
             <div class="build-main-center">
 
             </div>
             <div class="build-main-right">
+                <div class="bmr-header">
+                    <img src="../../images/build/close_right.png" alt="">
+                    <p></p>
+                </div>
+                <div class="bmr-content">
 
+                </div>
             </div>
         </div>
     </div>
